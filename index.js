dcb6a35ee734a3b3b202b06dcc07f7c8ac5cd44d
Dawid Maslankiewicz <maslankiewiczd@gmail.com>
2019-05-09 19:52:29 +0200

    Add gitignore change code

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 var fs = require('fs');
 //var StatMode = require('stat-mode');
 
-var newFile;
+
 
 /*fs.stat('./cat.jpg', function(err, stats) {
     var statMode = new StatMode(stats);
     console.log(statMode.toString());
 })*/
 
-fs.readdir('./node_modules','utf-8', function(err, files) {
+fs.readdir('./','utf-8', function(err, files) {
     
-    newFile = files;
-    console.log(newFile);
+   // newFile = files;
+    console.log(files);
     //return newFile;
-    fs.writeFile('./asd.txt', newFile, function(err) {
+    fs.writeFile('./asd.txt', files, function(err) {
         if (err) throw err; // jeśli pojawi się błąd, wyrzuć wyjątek
         console.log('Zapisano!');
     });
+    fs.readFile('./asd.txt', 'utf-8',function(err, data) {
+        console.log(data);
+        
+    })
 })
-fs.readFile('./asd.txt', 'utf-8',function(err, data) {
-    console.log(data);
-    
-})
+
 
 
 
